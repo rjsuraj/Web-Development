e2f2ea00ad1859fdf1cf66b228416052090ea659
Suraj Mandal <rjsurajmandal@gmail.com>
2023-10-05 15:50:02 +0530

    updated

--- Gsap/scrollTriggerProject/script.js
+++ Gsap/scrollTriggerProject/script.js
@@ -2,7 +2,7 @@
 function time(){
     let a = 0;
     let key = setInterval(() => {
-        a += Math.floor(Math.random()*15);
+        a += Math.floor(Math.random()*21);
         if(a >= 100){
             a = 100
             document.querySelector(".loader p").innerHTML = `${a}%`
@@ -16,7 +16,7 @@ let tl = gsap.timeline()
 
 tl.to(".loader p",{
     delay:0.5,
-    duration:1,
+    duration:1.5,
     onStart:time
 })
 .to(".loader",{
@@ -25,16 +25,17 @@ tl.to(".loader p",{
     duration:1
 })
 
-// gsap.to(".page1 h1",{
-//     transform: "translateX(-100%)",
-//     fontWeight: "100",
-//     scrollTrigger:{
-//         trigger:".page1",
-//         scroller:"body",
-//         markers:true,
-//         start:"top 0",
-//         end:"top -200%",
-//         scrub:2,
-//         pin:true
-//     }
-// })
+gsap.to(".page1 h1",{
+    transform: "translateX(-100%)",
+    fontWeight: "100",
+    scrollTrigger:{
+        trigger:".page1",
+        scroller:"body",
+        // markers:true,
+        start:"top 0",
+        end:"top -200%",
+        scrub:2,
+        pin:true
+    }
+})
+
